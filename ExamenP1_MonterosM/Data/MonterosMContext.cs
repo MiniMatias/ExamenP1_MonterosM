2b540cb42d68ac466c4de0e4ce4de83e7b674a3c
MiniMatias <matias.monteros@udla.edu.ec>
2025-04-29 08:27:38 -0500

    Se creo la carpeta data y se creo una clase llamada "MonterosM" para el manejo de datos, ademas de agrego un connection string en appsettings.json

---
+++ ExamenP1_MonterosM/Data/MonterosMContext.cs
@@ -0,0 +1,21 @@
+﻿using ExamenP1_MonterosM.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ExamenP1_MonterosM.Data 
+{
+    public class MonterosMContext : DbContext
+    {
+        public MonterosMContext(DbContextOptions<MonterosMContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; } = default!;
+        public DbSet<Reserva> Reservas { get; set; } = default!;
+        public DbSet<PlanRecompensas> PlanesRecompensas
+        { get; set; }
+            = default!;
+    }
+}
