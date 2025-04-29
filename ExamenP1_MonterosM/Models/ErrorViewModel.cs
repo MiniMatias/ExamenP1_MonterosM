5439cbd75f429c65f37e7c1f6b45e273820d72cf
laboratorio <laboratorio@UPOAULA10718>
2025-04-29 07:23:35 -0500

    Agregar archivos de proyecto.

---
+++ ExamenP1_MonterosM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExamenP1_MonterosM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
